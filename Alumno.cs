0220be3dec2d7dcb717feaff5b896ad29d22d590
Mario Bac <mariobacparedes@gmail.com>
2022-04-03 11:59:54 -0600

    Cambio 1

---
+++ Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationEjemplo1
+{
+    public class Alumno
+    {
+        public string carne { get; set; }
+
+        public string nombre { get; set; }
+    }
+}
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,58 @@ namespace WebApplicationEjemplo1
 {
     public partial class _Default : Page
     {
+        List<Alumno> alumnos = new List<Alumno>();
+        List<Inscripciones> inscripciones = new List<Inscripciones>();
         protected void Page_Load(object sender, EventArgs e)
         {
+            LeerAlumnos();
 
+            DropDownList1.DataValueField = "nombre";
+            DropDownList1.DataSource = alumnos;
+            DropDownList1.DataBind();
+        }
+        
+        private void LeerAlumnos()
+        {
+            string fileName = Server.MapPath("~/Archivos/Alumnos.txt");
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            while (reader.Peek() > -1)
+            {
+                Alumno alumno = new Alumno();
+                alumno.carne = reader.ReadLine();
+                alumno.nombre = reader.ReadLine();
+
+                alumnos.Add(alumno);
+            }
+
+            reader.Close();
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GuardarInscripciones(string fileName, string texto)
+        {
+            string fileName1  = Server.MapPath("~/Archivos/Inscripciones.txt");
+
+            FileStream stream = new FileStream(fileName1, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine(texto);
+            writer.Close();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Inscripciones inscripcion = new Inscripciones();
+
+            inscripcion.carne = DropDownList1.SelectedValue;
+            inscripcion.grado = Convert.ToInt16(((TextBox)sender).Text);
+            inscripcion.fecha = DateTime.Now;
+
+            inscripciones.Add(inscripcion);
         }
     }
 }
